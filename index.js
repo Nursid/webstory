f5b2a4341ea0db0683b367ebdc69e9647cdeaea9
Nursid Ansari <114871863+Nursid@users.noreply.github.com>
2024-06-23 23:47:55 +0530

    add all api

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const route= require("./src/routes")
+const bodyParser = require('body-parser');
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({  
+  extended: true
+}));
+
+app.use(cors());
+
+// Routes section
+const port = process.env.PORT || 5000;
+
+app.use("/api",route)
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -11,7 +26,10 @@ app.get('/about', (req, res) => {
   res.send('Hello About!');
 });
 
-// Start the server
+app.get('/nursid', (req, res) => {
+  res.send('Hello Nursid!');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
