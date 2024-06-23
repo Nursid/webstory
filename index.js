393945cbd88f45838f1557a6d408ba0fb9d7921a
Nursid Ansari <114871863+Nursid@users.noreply.github.com>
2024-06-23 23:22:08 +0530

    test

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const route= require("./src/routes")
-const bodyParser = require('body-parser');
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(cors());
-app.use(bodyParser.urlencoded({  
-  extended: true
-}));
-
-app.use(cors());
 
 // Routes section
-const port = process.env.PORT || 5000;
-
-app.use("/api",route)
+const port = 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-
-const userController = require('../controller/userController');
-router.get("/listing-users",userController.ListingUsers )
-
-    
-module.exports = router
